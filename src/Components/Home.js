353e9f16694f332e047dfe0555522f2b7ed37aa8
Sylvester Nwizu <nwizutoks@gmail.com>
2025-07-25 11:46:12 -0500

    Updated my about info

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -4,72 +4,89 @@ import resume from "../assets/resume/resume.pdf";
 function Home() {
 	return (
 		<>
-			<div className="row">
-				<div id="containerAbout" className="col-md-12 col-lg-12 text-left mx-4">
-					<h1 id="title">About Me</h1>
-					<img
-						src={profile}
-						id="profile"
-						width="200px"
-						height="200px"
-						style={{ borderRadius: "50%" }}
-						alt="TochukwuSylvesterNwizu"
-					/>
-					<p className="mt-3" id="Bio">
-						My name is Tochukwu Sylvester Nwizu. I attended the Vanderbilt
-						University coding Boot Camp. As an up coming full stack web
-						developer, I am willing to work in an environment that will allow me
-						to contribute as well as help me to grow and become a successful
-						full stack web developer. I have a very strong work ethic, and I
-						know the importance of continuously striving for improvement. For
-						this reason, I am open and willing to trying new things outside of
-						my comfort zone. I intend using my new learned skill set in solving
-						real world problems through collaboration with my coleques. I
-						believe with team work, no problem will be too difficult to tackle.
-						Moreover, My experience working in healthcare setting has helped me
-						to cultivate the habit of working calmly even in a highly stressful
-						environment.
-					</p>
-				</div>
-			</div>
+			<div className="d-flex justify-content-center">
+  <div
+    id="containerAbout"
+    style={{
+      maxWidth: "800px",
+      padding: "1rem",
+      textAlign: "left",
+    }}
+  >
+    <h1 id="title">About Me</h1>
+    <div className="text-center my-3">
+      <img
+        src={profile}
+        id="profile"
+        width="200px"
+        height="200px"
+        style={{ borderRadius: "50%" }}
+        alt="TochukwuSylvesterNwizu"
+      />
+    </div>
+
+    <div className="mt-3" id="Bio">
+      <p>
+        My name is Tochukwu Sylvester Nwizu, and I hold a Master of Science in
+        Computer Science with a concentration in Software Engineering from the
+        University of Tennessee, Knoxville. I have hands-on experience
+        developing enterprise applications, with proficiency in Java and
+        additional expertise in technologies such as JavaScript, Node.js, and
+        Python.
+      </p>
+      <p>
+        I thrive in collaborative environments where I can apply my technical
+        skills and continue to grow as an engineer. With a strong work ethic and
+        a mindset focused on continuous improvement, I actively seek out
+        opportunities that push me beyond my comfort zone and sharpen my ability
+        to solve real-world challenges through code.
+      </p>
+      <p>
+        My foundation in healthcare has equipped me to stay calm and focused
+        under pressure—skills that translate well into fast-paced and
+        high-stakes development environments. I believe that with strong
+        teamwork and intentional problem-solving, even the most complex
+        technical challenges can be tackled with clarity and impact.
+      </p>
+    </div>
+  </div>
+</div>
 
 			<div id="links" className="col-md-12">
-				<a href={resume} target="_blank">
+				<a href={resume} target="_blank" rel="noopener noreferrer">
 					<svg
 						xmlns="http://www.w3.org/2000/svg"
 						width="50"
 						height="50"
 						fill="currentColor"
-						class="bi bi-file-earmark-person"
+						className="bi bi-file-earmark-person"
 						viewBox="0 0 16 16"
 					>
 						<path d="M11 8a3 3 0 1 1-6 0 3 3 0 0 1 6 0z" />
 						<path d="M14 14V4.5L9.5 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2zM9.5 3A1.5 1.5 0 0 0 11 4.5h2v9.255S12 12 8 12s-5 1.755-5 1.755V2a1 1 0 0 1 1-1h5.5v2z" />
 					</svg>
 				</a>
-				<a href="https://github.com/toksly" target="_blank" className="mx-3">
-					{/* <!-- <img class="linkImg" src="./assets/images/github.png" alt="project"/> -- */}
+
+				<a href="https://github.com/toksly" target="_blank" rel="noopener noreferrer" className="mx-3">
 					<svg
 						xmlns="http://www.w3.org/2000/svg"
 						width="50"
 						height="50"
 						fill="currentColor"
-						class="bi bi-github"
+						className="bi bi-github"
 						viewBox="0 0 16 16"
 					>
 						<path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z" />
 					</svg>
 				</a>
-				<a
-					href="https://www.linkedin.com/in/sylvester-nwizu-6151b3aa/"
-					target="_blank"
-				>
+
+				<a href="https://www.linkedin.com/in/sylvester-nwizu-6151b3aa/" target="_blank" rel="noopener noreferrer">
 					<svg
 						xmlns="http://www.w3.org/2000/svg"
 						width="50"
 						height="50"
 						fill="currentColor"
-						class="bi bi-linkedin"
+						className="bi bi-linkedin"
 						viewBox="0 0 16 16"
 					>
 						<path d="M0 1.146C0 .513.526 0 1.175 0h13.65C15.474 0 16 .513 16 1.146v13.708c0 .633-.526 1.146-1.175 1.146H1.175C.526 16 0 15.487 0 14.854V1.146zm4.943 12.248V6.169H2.542v7.225h2.401zm-1.2-8.212c.837 0 1.358-.554 1.358-1.248-.015-.709-.52-1.248-1.342-1.248-.822 0-1.359.54-1.359 1.248 0 .694.521 1.248 1.327 1.248h.016zm4.908 8.212V9.359c0-.216.016-.432.08-.586.173-.431.568-.878 1.232-.878.869 0 1.216.662 1.216 1.634v3.865h2.401V9.25c0-2.22-1.184-3.252-2.764-3.252-1.274 0-1.845.7-2.165 1.193v.025h-.016a5.54 5.54 0 0 1 .016-.025V6.169h-2.4c.03.678 0 7.225 0 7.225h2.4z" />
